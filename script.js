9e6affaf160b46d1f8f7bfa7fd992f468f1dc56f
Morgan Causey <morganelizabethcausey@gmail.com>
2023-11-08 12:19:58 -0500

    css file update

---
+++ script.js
@@ -0,0 +1,5 @@
+const statusDisplay = document.querySelector('.game-status');
+let gameActive = true;
+let currentPlayer = "X";
+let gameState = ["", "", "", "", "", "", "", "",""];
+const winningMessage = () = 
